d728a91d3bc54105882d504ecc6e87e0d98b1013
pakphattadet <108240904+wnevy@users.noreply.github.com>
2025-05-17 13:28:03 +0700

    Add files via upload

---
+++ pakphattadet_yingjimroensart.ts
@@ -0,0 +1,35 @@
+const portals = [
+  { location: 55, destination: 38 },
+  { location: 14, destination: 35 },
+  { location: 91, destination: 48 },
+  { location: 30, destination: 8 },
+  { location: 31, destination: 70 },
+  { location: 63, destination: 83 },
+  { location: 3, destination: 39 },
+  { location: 47, destination: 86 },
+  { location: 71, destination: 93 },
+  { location: 21, destination: 4 },
+  { location: 44, destination: 65 },
+  { location: 96, destination: 66 },
+  { location: 79, destination: 42 },
+  { location: 87, destination: 54 },
+  { location: 90, destination: 119 },
+  { location: 120, destination: 149 },
+  { location: 150, destination: 179 },
+  { location: 180, destination: 200 },
+];
+
+quickestPath(portals);
+
+function quickestPath(portals: { location: number; destination: number }[]): number {
+    portals.map(item => item.destination)
+
+    return 0;
+}
+
+// random location 1-11
+//
+
+function quickestPath2({ protals: {location: number}}) {
+    
+}
